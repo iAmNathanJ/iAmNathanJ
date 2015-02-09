4fea68e27a804e0f8c0d24acc878ed3ac8fc4037
Nathan Jacobs <jacobs.123@gmail.com>
2015-02-08 19:38:13 -0500

    updating .gitignore

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('login');
 });
-
+router.get('/user', function(req, res, next) {
+  res.render('user');
+});
 module.exports = router;
