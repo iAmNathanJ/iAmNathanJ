cabf6de6cebb9135d13ea54bab97e819cd3baed6
Nathan Jacobs <jacobs.123@gmail.com>
2016-02-07 20:59:00 -0500

    update game-of-life.js: working state generations on end point

--- dev/scripts/play-game-of-life.js
+++ dev/scripts/play-game-of-life.js
@@ -1,6 +1,47 @@
 (function() {
 
-  var x = 'hello';
-  console.log(x);
+  'use strict';
+
+  // SERIALIZE HELPER
+  function serialize(data) {
+
+    let queryString = [];
+    let dataKeys = Object.keys(data);
+
+    let add = (key, val) => {
+      queryString.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
+    };
+
+    let addArray = (key, arr) => {
+      arr.forEach((val) => {
+        queryString.push(`${encodeURIComponent(key)}[]=${encodeURIComponent(val)}`);
+      });
+    };
+
+    dataKeys.forEach((key, i) => {
+      if(Array.isArray(data[key])) {
+        addArray(key, data[key])
+      } else {
+        add(key, data[key]);
+      }
+    });
+    return queryString.join('&');
+  }
+
+  (function generateLife(data) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', `/game-of-life/api?${data}`);
+    xhr.send();
+    xhr.addEventListener('load', (e) => {
+
+      let msg = JSON.parse(e.target.response);
+      console.log(msg);
+
+      let newData = { state: msg };
+      setTimeout(() => {
+        generateLife(serialize(newData));
+      }, 2000);
+    });
+  })();
 
 })();
--- public/scripts/play-game-of-life.js
+++ public/scripts/play-game-of-life.js
@@ -1,1 +1,47 @@
-!function(){var o="hello";console.log(o)}();
+(function() {
+
+  'use strict';
+
+  // SERIALIZE HELPER
+  function serialize(data) {
+
+    let queryString = [];
+    let dataKeys = Object.keys(data);
+
+    let add = (key, val) => {
+      queryString.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
+    };
+
+    let addArray = (key, arr) => {
+      arr.forEach((val) => {
+        queryString.push(`${encodeURIComponent(key)}[]=${encodeURIComponent(val)}`);
+      });
+    };
+
+    dataKeys.forEach((key, i) => {
+      if(Array.isArray(data[key])) {
+        addArray(key, data[key])
+      } else {
+        add(key, data[key]);
+      }
+    });
+    return queryString.join('&');
+  }
+
+  (function generateLife(data) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', `/game-of-life/api?${data}`);
+    xhr.send();
+    xhr.addEventListener('load', (e) => {
+
+      let msg = JSON.parse(e.target.response);
+      console.log(msg);
+
+      let newData = { state: msg };
+      setTimeout(() => {
+        generateLife(serialize(newData));
+      }, 2000);
+    });
+  })();
+
+})();
