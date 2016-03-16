a38d09314a2bd0c85af62218549cbdcadbcc5396
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-16 13:16:21 -0400

    cleanup

--- public/scripts/game-of-life.js
+++ public/scripts/game-of-life.js
@@ -3,15 +3,6 @@
 
 const life = require('./lib/game-of-life');
 
-life.seed([
-  { x: 0, y: 0 },
-  { x: 1, y: 0 },
-  { x: 0, y: 1 }
-]);
-
-life.generate();
-console.log(life.state);
-
 },{"./lib/game-of-life":2}],2:[function(require,module,exports){
 'use strict';
 
